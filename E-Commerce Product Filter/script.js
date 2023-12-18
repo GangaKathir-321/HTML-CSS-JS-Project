f7d93d312456b190807344075706011cc252bf04
GangaKathir-321 <152600973+GangaKathir-321@users.noreply.github.com>
2023-12-18 15:48:26 +0530

    E-Commerce

---
+++ E-Commerce Product Filter/script.js
@@ -0,0 +1,281 @@
+const data = [
+  {
+    id: 1,
+    name: "pTon ZenBuds",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51myi+sPEoL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 1199,
+    seller: "pTon Store",
+    catagory: "Earbuds",
+  },
+  {
+    id: 2,
+    name: "pTon Buds",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41OZk52JoeL._AC_UX450_UY450_SR450,450_.jpg",
+    amt: 1500,
+    seller: "pTon Store",
+    catagory: "Earbuds",
+  },
+
+  {
+    id: 3,
+    name: "Noise Buds",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/615-1qhF7DL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 1489,
+    seller: "Noise store",
+    catagory: "Earbuds",
+  },
+  {
+    id: 4,
+    name: "Sony WH-CH520",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/31eo70r2NWL._SX300_SY300_QL70_FMwebp_.jpg",
+    amt: 5099,
+    seller: "Sony store",
+    catagory: "Headphone",
+  },
+  {
+    id: 5,
+    name: "Oneplus Bullets Z2",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/519ubHUdP3L._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 1499,
+    seller: "oneplus store",
+    catagory: "Bluetooth",
+  },
+
+  {
+    id: 6,
+    name: "Realme Buds",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51Pvn6341QL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 3999,
+    seller: "Realme store",
+    catagory: "Bluetooth",
+  },
+
+  {
+    id: 7,
+    name: "iClever Headphone",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61akGsYRuQL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 1599,
+    seller: "iClever Store",
+    catagory: "Headphone",
+  },
+  {
+    id: 8,
+    name: "boAt RockerZ 450",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/619gMYaALcL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 1699,
+    seller: "boAt Store",
+    catagory: "Headphone",
+  },
+  {
+    id: 9,
+    name: "Bassheads 225",
+    img: "https://m.media-amazon.com/images/I/61gs3SGzk4L._AC_UF350,350_QL50_.jpg",
+    amt: 699,
+    seller: "boAt Store",
+    catagory: "Headset",
+  },
+
+  {
+    id: 10,
+    name: "Xiaomi 123",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51JEyMBwVZL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 500,
+    seller: " Xiaomi store",
+    catagory: "Headset",
+  },
+  {
+    id: 11,
+    name: "boAt Bassheads 100",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61W3UH0AYXL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 799,
+    seller: " boAt store",
+    catagory: "Headset",
+  },
+  {
+    id: 12,
+    name: "oneplus Bullets Z9",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51Kf2YhpgVL._AC_UF894,1000_QL80_FMwebp_.jpg",
+    amt: 1499,
+    seller: "oneplus store",
+    catagory: "Bluetooth",
+  },
+
+  {
+    id: 13,
+    name: "JBL Wave Beam(TWZ)",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/614hH1Cot3L._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 3199,
+    seller: "JBL store",
+    catagory: "Earbuds",
+  },
+  {
+    id: 14,
+    name: "Infinity-JBL Glide N120",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51Hf-obS0zL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 4999,
+    seller: "Infinity store",
+    catagory: "Bluetooth",
+  },
+  {
+    id: 15,
+    name: "pTon Tangentbeat",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61BaiML3MzL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 600,
+    seller: "pTon store",
+    catagory: "Headphone",
+  },
+  {
+    id: 16,
+    name: " boAt Bassheads",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/519kP1zuVRL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 699,
+    seller: "boAt Store",
+    catagory: "Headset",
+  },
+  {
+    id: 17,
+    name: "iClever Headphone",
+    img: "https://m.media-amazon.com/images/I/51cv0E1mywL._AC_UF350,350_QL50_.jpg",
+    amt: 2099,
+    seller: "iClever Store",
+    catagory: "Headphone",
+  },
+  {
+    id: 18,
+    name: "Noise Buds",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/619gHpSiOXL._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 1594,
+    seller: "Noise store",
+    catagory: "Earbuds",
+  },
+  {
+    id: 19,
+    name: "Oneplus Bullets",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41NYkpCPxlL._AC_UX450_UY450_SR450,450_.jpg",
+    amt: 1499,
+    seller: "oneplus store",
+    catagory: "Bluetooth",
+  },
+  {
+    id: 20,
+    name: "iClever Headphone",
+    img: "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51snNPl07ML._AC_UF1000,1000_QL80_FMwebp_.jpg",
+    amt: 999,
+    seller: "iclever store",
+    catagory: "Headphone",
+  },
+];
+
+const productsContainer = document.querySelector(".products");
+const categoryList = document.querySelector(".category-list");
+
+function displayProducts(products) {
+  if (products.length > 0) {
+    const product_details = products
+      .map(
+        (product) => `
+  <div class="product">
+  <div class="img">
+    <img src="${product.img}" alt="${product.name}" />
+  </div>
+  <div class="product-details">
+    <span class="name">${product.name}</span>
+    <span class="amt">Rs.${product.amt}</span>
+    <span class="seller">${product.seller}</span>
+  </div>
+</div>`
+      )
+      .join("");
+
+    productsContainer.innerHTML = product_details;
+  } else {
+    productsContainer.innerHTML = "<h3>No Products Available</h3>";
+  }
+}
+
+function setCategories() {
+  const allCategories = data.map((product) => product.catagory);
+  //console.log(allCategories);
+  const catagories = [
+    "All",
+    ...allCategories.filter((product, index) => {
+      return allCategories.indexOf(product) === index;
+    }),
+  ];
+  //console.log(catagories);
+  categoryList.innerHTML = catagories
+    .map((catagory) => `<li>${catagory}</li>`)
+    .join("");
+
+  categoryList.addEventListener("click", (e) => {
+    const selectedCatagory = e.target.textContent;
+    selectedCatagory === "All"
+      ? displayProducts(data)
+      : displayProducts(
+          data.filter((product) => product.catagory == selectedCatagory)
+        );
+  });
+}
+const priceRange = document.querySelector("#priceRange");
+const priceValue = document.querySelector(".priceValue");
+
+function setPrices() {
+  const priceList = data.map((product) => product.amt);
+  const minPrice = Math.min(...priceList);
+  const maxPrice = Math.max(...priceList);
+  priceRange.min = minPrice;
+  priceRange.max = maxPrice;
+  priceValue.textContent = "Rs." + maxPrice;
+
+  priceRange.addEventListener("input", (e) => {
+    priceValue.textContent = "Rs." + e.target.value;
+    displayProducts(data.filter((product) => product.amt <= e.target.value));
+  });
+}
+
+const txtSearch = document.querySelector("#txtSearch");
+txtSearch.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase().trim();
+  if (value) {
+    displayProducts(
+      data.filter((product) => product.name.toLowerCase().indexOf(value) !== -1)
+    );
+  } else {
+    displayProducts(data);
+  }
+});
+
+displayProducts(data);
+setCategories();
+setPrices();
+
+/*
+
+https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/619gHpSiOXL._AC_UF1000,1000_QL80_FMwebp_.jpg.buds
+
+ttps://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/51snNPl07ML._AC_UF1000,1000_QL80_FMwebp_.jpg
+
+Headphone
+
+https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/41NYkpCPxlL._AC_UX450_UY450_SR450,450_.jpg
+
+Bluetooth
+
+https://m.media-amazon.com/images/I/51fMTxsbz+S._AC_UF350,350_QL50_.jpg
+
+Headset
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
